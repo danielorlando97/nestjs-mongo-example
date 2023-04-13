2eb3e1294cf7e40f1cf349c971915fe5f8d76bad
danielorlando97 <danielorlandoortizpacheco@gmail.com>
2023-04-13 11:00:00 -0400

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,20 @@ import { Module } from '@nestjs/common';
 import { ModulesModule } from './modules/modeles.module';
 import { configService } from './db.config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { BullModule } from '@nestjs/bull';
+
 @Module({
   imports: [
     ModulesModule,
     configService.getDBConfig(),
     EventEmitterModule.forRoot(),
+
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
   ],
 })
 export class AppModule {}
