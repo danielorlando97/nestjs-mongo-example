e4c5233d2e312a8294ad3a9e2cb07a6a11a272db
danielorlando97 <danielorlandoortizpacheco@gmail.com>
2023-04-13 09:18:48 -0400

    add event clients

--- src/modules/client/client.service.ts
+++ src/modules/client/client.service.ts
@@ -4,10 +4,14 @@ import { CreateClientDto } from './dtos/client.create.dto';
 import { UpdateClientDto } from './dtos/client.update.dto';
 import { Model } from 'mongoose';
 import { Client, ClientDocument } from './client.entity';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 
 @Injectable()
 export class ClientService {
-  constructor(@InjectModel(Client.name) private model: Model<ClientDocument>) {}
+  constructor(
+    @InjectModel(Client.name) private model: Model<ClientDocument>,
+    private eventEmitter: EventEmitter2,
+  ) {}
 
   async create(dto: CreateClientDto): Promise<Client> {
     const newEntity = await new this.model(dto);
@@ -46,6 +50,9 @@ export class ClientService {
     if (!deletedEntity) {
       throw new NotFoundException(`Client #${id} not found`);
     }
+
+    this.eventEmitter.emit('client.deleted', deletedEntity._id);
+
     return deletedEntity;
   }
 }
