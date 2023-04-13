da52a5608545eb9a7bab97a27cadacc3a7724922
danielorlando97 <danielorlandoortizpacheco@gmail.com>
2023-04-13 10:59:46 -0400

    update modules

--- src/modules/modeles.module.ts
+++ src/modules/modeles.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClientModule } from './client/client.module';
 import { RestaurantModule } from './restaurant/restaurant.module';
-import { OfferModule } from './offer/offer.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [OfferModule],
+  imports: [OrderModule],
 })
 export class ModulesModule {}
